aebb06b6b8552c59ca6bbf3a812988a28eed4201
Inhwan Jung <inhwanjung@Inhwanui-MacBookPro.local>
2023-02-09 20:55:09 +0900

    setting 폼 수정

--- resources/views/setting/partials/update-setting-infomation-form.blade.php
+++ resources/views/setting/partials/update-setting-infomation-form.blade.php
@@ -18,33 +18,15 @@
         @method('patch')
 
         <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name" />
-            <x-input-error class="mt-2" :messages="$errors->get('name')" />
+            <x-input-label for="mqv_id" :value="__('MQV_ID')" />
+            <x-text-input id="mqv_id" name="mqv_id" type="text" class="mt-1 block w-full" :value="old('mqv_id', $user->setting()?->mqv_id ?? '')" required autofocus autocomplete="name" />
+            <x-input-error class="mt-2" :messages="$errors->get('mqv_id')" />
         </div>
 
         <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" name="email" type="email" class="mt-1 block w-full" :value="old('email', $user->email)" required autocomplete="username" />
-            <x-input-error class="mt-2" :messages="$errors->get('email')" />
-
-            @if ($user instanceof \Illuminate\Contracts\Auth\MustVerifyEmail && ! $user->hasVerifiedEmail())
-            <div>
-                <p class="text-sm mt-2 text-gray-800">
-                    {{ __('Your email address is unverified.') }}
-
-                    <button form="send-verification" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
-                        {{ __('Click here to re-send the verification email.') }}
-                    </button>
-                </p>
-
-                @if (session('status') === 'verification-link-sent')
-                <p class="mt-2 font-medium text-sm text-green-600">
-                    {{ __('A new verification link has been sent to your email address.') }}
-                </p>
-                @endif
-            </div>
-            @endif
+            <x-input-label for="mqv_password" :value="__('MQV_PASSWORD')" />
+            <x-text-input id="mqv_password" name="mqv_password" type="password" class="mt-1 block w-full" :value="old('mqv_password', $user->setting()?->mqv_password ?? '')" required autocomplete="username" />
+            <x-input-error class="mt-2" :messages="$errors->get('mqv_password')" />
         </div>
 
         <div class="flex items-center gap-4">
