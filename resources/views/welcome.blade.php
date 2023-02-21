5ced9389ce514b60b8a73eb7770c5c0a56e7490d
정인환 <balt11@naver.com>
2023-02-21 15:36:11 +0900

    메인 페이지 소개

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
-                    <img src="https://connectwave.co.kr/img/logo.png">
+                    <img src="//connectwave.co.kr/img/logo.png">
                 </div>
 
                 <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
