26134fda3e0d2b0952de9deddb2b0b9630d0a545
정인환 <balt11@naver.com>
2023-05-02 10:17:55 +0900

    가산 오피스 출근으로 인하여 스케줄러 주석 처리

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -18,17 +18,17 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(MqvReservationCommand::class)
-            ->dailyAt('12:00')
-            ->description('MQV 자리 예약')
-            ->runInBackground()
-            ->WithoutOverlapping();
+        // $schedule->command(MqvReservationCommand::class)
+        //     ->dailyAt('12:00')
+        //     ->description('MQV 자리 예약')
+        //     ->runInBackground()
+        //     ->WithoutOverlapping();
 
-        $schedule->command(MqvSeatCommand::class)
-            ->weeklyOn(1, '09:30')
-            ->description('MQV 좌석 크롤링')
-            ->runInBackground()
-            ->WithoutOverlapping();
+        // $schedule->command(MqvSeatCommand::class)
+        //     ->weeklyOn(1, '09:30')
+        //     ->description('MQV 좌석 크롤링')
+        //     ->runInBackground()
+        //     ->WithoutOverlapping();
     }
 
     /**
